8ce2ae15565f86bf6e6c4af23084b2fa29907e91
Isuu1 <jakub.babelek@gmail.com>
2025-02-02 19:06:49 +0000

    Old styles cleanup + created variables for colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Neucha } from "next/font/google";
-import "./globals.scss";
+import "@/styles/globals.scss";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import { StorageProvider } from "@/context/StorageContext";

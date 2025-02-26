a908679d6de200b2bd20431d8520d6e10bc29a12
Isuu1 <jakub.babelek@gmail.com>
2025-02-26 12:50:59 +0000

    Added attribution in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //Styles
 import styles from "./Footer.module.scss";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -9,6 +10,15 @@ const Footer = () => {
       <h3 className={styles.copyright}>
         &copy; 2025 Gifinity. All rights reserved.
       </h3>
+      {/* <h3 className={styles.attribution}>Powered By GIPHY</h3> */}
+      <Image
+        src="/images/attribution.gif"
+        alt="Giphy Logo"
+        width={200}
+        height={42}
+        className={styles.attribution}
+        unoptimized
+      />
     </footer>
   );
 };

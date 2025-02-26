f52045d1078e664efca3c47196c74a4bcf6c3a65
Isuu1 <jakub.babelek@gmail.com>
2025-02-26 12:56:26 +0000

    Removed caching from trending data fetch to ensure data is always fresh

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,8 @@
 async function fetchData(endpoint: string) {
   try {
     const data = await fetch(
-      `https://api.giphy.com/v1/${endpoint}?api_key=${process.env.API_KEY}`
+      `https://api.giphy.com/v1/${endpoint}?api_key=${process.env.API_KEY}`,
+      { cache: "no-store" }
     );
     const response = await data.json();
     return response;

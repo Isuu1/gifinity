9b85105547ec1e1a4c94f9c39dd15e6dd408e139
Isuu1 <jakub.babelek@gmail.com>
2025-02-26 11:49:17 +0000

    Moved fetch trending tag function to separate file

--- utils/client.ts
+++ utils/client.ts
@@ -15,3 +15,20 @@ export async function fetchCategoryData(categoryName: string, type: string) {
     return { data: null, error: `Error fetching gifs: ${error}` };
   }
 }
+
+export async function fetchTrendingByTag(searchQuery: string, type: string) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/${type}/search?q=${searchQuery}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error: `Error fetching gifs: ${error}` };
+  }
+}

08f4a25ab0d790e6c691cb304a82b3821586823b
Isuu1 <jakub.babelek@gmail.com>
2025-02-27 10:29:17 +0000

    Added favourites page

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -2,6 +2,21 @@ export default function Page() {
   return (
     <div className="page">
       <h2>Favourites</h2>
+      <div
+        style={{
+          background: "#a0153e",
+          padding: "1rem",
+          borderRadius: "24px",
+          color: "#fff",
+          margin: "1rem auto",
+        }}
+      >
+        <h4>
+          Your favorites are currently stored in your browser`s local storage.
+          Creating an account will allow you to save them permanently allowing
+          you to access then on any device.
+        </h4>
+      </div>
     </div>
   );
 }

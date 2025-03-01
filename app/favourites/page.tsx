d0f829ee1b7b2bcce62c30467ec43e36ee844dc8
Isuu1 <jakub.babelek@gmail.com>
2025-03-01 15:17:42 +0000

    Added separate component for Favourites feed

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 
 //Components
-import DataFeed from "@/components/DataFeed/DataFeed";
+import FavouritesFeed from "@/components/FavouritesFeed/FavouritesFeed";
 
 //Context
 import { useStorage } from "@/context/StorageContext";
@@ -16,31 +16,10 @@ export default function Page() {
         <Image src="/images/heart.svg" alt="trending" width={40} height={40} />
         <h2 className="headline-container__text">Favourites</h2>
       </div>
-      <div
-        style={{
-          background: "#a0153e",
-          padding: "1rem",
-          borderRadius: "24px",
-          color: "#fff",
-          margin: "1rem auto",
-        }}
-      >
-        <h4>
-          Your favorites are currently stored in your browser`s local storage.
-          Creating an account will allow you to save them permanently allowing
-          you to access them on any device.
-        </h4>
-      </div>
-      {localFavouriteGifs.data.length > 0 ||
-      localFavouriteStickers.data.length > 0 ? (
-        <DataFeed
-          data={{ gifs: localFavouriteGifs, stickers: localFavouriteStickers }}
-        />
-      ) : (
-        <h2 style={{ textAlign: "center" }}>
-          You don`t have any favourites yet!
-        </h2>
-      )}
+
+      <FavouritesFeed
+        data={{ gifs: localFavouriteGifs, stickers: localFavouriteStickers }}
+      />
     </div>
   );
 }
---
+++ components/FavouritesFeed/FavouritesFeed.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+
+//Interfaces
+import { Gifs } from "@/interfaces/gifs";
+import { Stickers } from "@/interfaces/stickers";
+
+//Components
+import Button from "../UI/Button";
+import MediaOverlay from "../MediaOverlay/MediaOverlay";
+
+//Styles
+import styles from "./FavouritesFeed.module.scss";
+
+//Animations
+import { AnimatePresence } from "motion/react";
+
+interface IProps {
+  data: {
+    gifs?: Gifs;
+    stickers?: Stickers;
+  };
+}
+
+const FavouritesFeed: React.FC<IProps> = ({ data }) => {
+  const { gifs, stickers } = data;
+
+  const [showOverlay, setShowOverlay] = useState<string | null>(null);
+
+  const [activeButton, setActiveButton] = useState<string>("gifs");
+
+  const [displayedContent, setDisplayedContent] = useState<
+    Gifs | Stickers | undefined
+  >(gifs);
+
+  const isFavouritesEmpty = !displayedContent?.data.length;
+
+  useEffect(() => {
+    // Ensure it updates when props change
+    setDisplayedContent(activeButton === "gifs" ? gifs : stickers);
+  }, [gifs, stickers, activeButton]);
+
+  return (
+    <div>
+      <div className={styles.notification}>
+        <h4>
+          Your favorites are currently stored in your browser`s local storage.
+          Creating an account will allow you to save them permanently allowing
+          you to access them on any device.
+        </h4>
+      </div>
+      <div className={styles.submenuContainer}>
+        <Button
+          active={activeButton === "gifs" && true}
+          onClick={() => setActiveButton("gifs")}
+        >
+          Gifs
+        </Button>
+        <Button
+          active={activeButton === "stickers" && true}
+          onClick={() => setActiveButton("stickers")}
+        >
+          Stickers
+        </Button>
+      </div>
+      <div className={styles.feedContainer}>
+        {displayedContent &&
+          displayedContent.data.map((media) => (
+            <div
+              key={media.id}
+              className={styles.gif}
+              onMouseEnter={() => setShowOverlay(media.id)}
+              onMouseLeave={() => setShowOverlay(null)}
+            >
+              <AnimatePresence initial={false}>
+                {showOverlay === media.id && (
+                  <MediaOverlay key={media.id} media={media} />
+                )}
+              </AnimatePresence>
+              <Image
+                className={styles.image}
+                src={media.images.original.url}
+                alt={media.title}
+                fill
+                unoptimized
+              />
+            </div>
+          ))}
+      </div>
+      {isFavouritesEmpty && (
+        <div className={styles.emptyFavourites}>
+          <h3>You don`t have any favourite {activeButton} yet!</h3>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FavouritesFeed;

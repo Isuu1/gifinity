e0cc8ef7094f8066f4bf15b717d998836caacddb
Isuu1 <jakub.babelek@gmail.com>
2025-03-01 11:40:18 +0000

    Removed old code from categories menu

--- components/CategoriesMenu/CategoriesMenu.tsx
+++ components/CategoriesMenu/CategoriesMenu.tsx
@@ -44,26 +44,7 @@ const CategoriesMenu: React.FC = () => {
     setShowCategories(!showCategories);
   };
 
-  //In category name replace & with -
-  // const replaceAnd = (categoryName: string) => {
-  //   const slicedCategoryName = categoryName.substring(
-  //     0,
-  //     categoryName.indexOf("&")
-  //   );
-  //   return slicedCategoryName;
-  // };
-
   const handleCategoryChange = (categoryName: string) => {
-    // if (categoryName.includes("&")) {
-    //   const newCategoryName = replaceAnd(categoryName);
-    //   //router push path with category name
-    //   router.push(`/category?q=${newCategoryName}`);
-    //   return newCategoryName;
-    // } else {
-    //   //router push path with category name
-    //   router.push(`/category?q=${categoryName}`);
-    //   return categoryName;
-    // }
     router.push(`/category?q=${categoryName}`);
   };
 

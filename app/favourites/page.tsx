5ae2042e773a0d5f0cec6784fef47dc5a6fd7ff0
Isuu1 <jakub.babelek@gmail.com>
2025-03-02 15:30:37 +0000

    Moved some components and states out of FavouritesFeed to page.tsx

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 
 //Components
 import FavouritesFeed from "@/components/FavouritesFeed/FavouritesFeed";
+import NotificationMessage from "@/components/NotificationMessage/NotificationMessage";
+import MediaTypeMenu from "@/components/MediaTypeMenu/MediaTypeMenu";
+import Button from "@/components/UI/Button";
+import Modal from "@/components/Modal/Modal";
 
 //Context
 import { useStorage } from "@/context/StorageContext";
 
+//Icons
+import { IoTrashBin } from "react-icons/io5";
+
+//Animations
+import { AnimatePresence } from "motion/react";
+
 export default function Page() {
-  const { localFavouriteGifs, localFavouriteStickers } = useStorage();
+  const {
+    localFavouriteGifs,
+    localFavouriteStickers,
+    removeFavouritesFromLocalStorage,
+  } = useStorage();
+
+  const [activeButton, setActiveButton] = useState<string>("gifs");
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const handleRemoveItems = () => {
+    removeFavouritesFromLocalStorage();
+    setShowModal(false);
+  };
 
   return (
     <div className="page">
@@ -17,8 +41,47 @@ export default function Page() {
         <h2 className="headline-container__text">Favourites</h2>
       </div>
 
+      <AnimatePresence initial={false}>
+        {showModal && (
+          <Modal key="modal">
+            <h3>
+              Are you sure you want to delete favourites gifs and stickers?
+            </h3>
+            <div className="flex-row">
+              <Button active onClick={() => handleRemoveItems()}>
+                Confirm
+              </Button>
+              <Button onClick={() => setShowModal(false)}>Cancel</Button>
+            </div>
+          </Modal>
+        )}
+      </AnimatePresence>
+
+      <NotificationMessage>
+        Your favorites are currently stored in your browser`s local storage.
+        Creating an account will allow you to save them permanently allowing you
+        to access them on any device.
+      </NotificationMessage>
+
+      <div className="flex-row">
+        <MediaTypeMenu
+          activeButton={activeButton}
+          setActiveButton={setActiveButton}
+        />
+        <div className="margin-left-auto">
+          <Button
+            icon={<IoTrashBin />}
+            iconPosition="right"
+            onClick={() => setShowModal(true)}
+          >
+            Clear favourites
+          </Button>
+        </div>
+      </div>
+
       <FavouritesFeed
         data={{ gifs: localFavouriteGifs, stickers: localFavouriteStickers }}
+        activeButton={activeButton}
       />
     </div>
   );
--- components/FavouritesFeed/FavouritesFeed.tsx
+++ components/FavouritesFeed/FavouritesFeed.tsx
@@ -6,10 +6,7 @@ import { Gifs } from "@/interfaces/gifs";
 import { Stickers } from "@/interfaces/stickers";
 
 //Components
-import Button from "../UI/Button";
 import MediaOverlay from "../MediaOverlay/MediaOverlay";
-import NotificationMessage from "../NotificationMessage/NotificationMessage";
-import Modal from "../Modal/Modal";
 
 //Styles
 import styles from "./FavouritesFeed.module.scss";
@@ -17,41 +14,24 @@ import styles from "./FavouritesFeed.module.scss";
 //Animations
 import { AnimatePresence } from "motion/react";
 
-//Icons
-import { IoTrashBin } from "react-icons/io5";
-
-//Context
-import { useStorage } from "@/context/StorageContext";
-import MediaTypeMenu from "../MediaTypeMenu/MediaTypeMenu";
-
 interface IProps {
   data: {
     gifs?: Gifs;
     stickers?: Stickers;
   };
+  activeButton: string;
 }
 
-const FavouritesFeed: React.FC<IProps> = ({ data }) => {
+const FavouritesFeed: React.FC<IProps> = ({ data, activeButton }) => {
   const { gifs, stickers } = data;
 
   const [showOverlay, setShowOverlay] = useState<string | null>(null);
-
-  const [activeButton, setActiveButton] = useState<string>("gifs");
 
   const [displayedContent, setDisplayedContent] = useState<
     Gifs | Stickers | undefined
   >(gifs);
 
-  const [showModal, setShowModal] = useState<boolean>(false);
-
-  const { removeFavouritesFromLocalStorage } = useStorage();
-
   const isFavouritesEmpty = !displayedContent?.data.length;
-
-  const handleRemoveItems = () => {
-    removeFavouritesFromLocalStorage();
-    setShowModal(false);
-  };
 
   useEffect(() => {
     // Ensure it updates when props change
@@ -60,42 +40,6 @@ const FavouritesFeed: React.FC<IProps> = ({ data }) => {
 
   return (
     <div>
-      <AnimatePresence initial={false}>
-        {showModal && (
-          <Modal key="modal">
-            <h3>
-              Are you sure you want to delete favourites gifs and stickers?
-            </h3>
-            <div className="flex-row">
-              <Button active onClick={() => handleRemoveItems()}>
-                Confirm
-              </Button>
-              <Button onClick={() => setShowModal(false)}>Cancel</Button>
-            </div>
-          </Modal>
-        )}
-      </AnimatePresence>
-      <NotificationMessage>
-        Your favorites are currently stored in your browser`s local storage.
-        Creating an account will allow you to save them permanently allowing you
-        to access them on any device.
-      </NotificationMessage>
-
-      <div className="flex-row">
-        <MediaTypeMenu
-          activeButton={activeButton}
-          setActiveButton={setActiveButton}
-        />
-        <div className={styles.clearButton}>
-          <Button
-            icon={<IoTrashBin />}
-            iconPosition="right"
-            onClick={() => setShowModal(true)}
-          >
-            Clear favourites
-          </Button>
-        </div>
-      </div>
       <div className={styles.feedContainer}>
         {displayedContent &&
           displayedContent.data.map((media) => (

0ef6fb16342d2afc92911d6a40e36b3e0fd3286f
Isuu1 <jakub.babelek@gmail.com>
2025-03-02 16:26:57 +0000

    Added styling to confirmDelete component

--- components/ConfirmDelete/ConfirmDelete.tsx
+++ components/ConfirmDelete/ConfirmDelete.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+
+//Components
 import Button from "../UI/Button";
+
+//Styles
+import styles from "./ConfirmDelete.module.scss";
 
 interface IProps {
   onConfirm: () => void;
@@ -8,7 +13,7 @@ interface IProps {
 
 const ConfirmDelete: React.FC<IProps> = ({ onConfirm, onCancel }) => {
   return (
-    <div>
+    <div className={styles.confirmDelete}>
       <h3>Are you sure you want to delete favourites gifs and stickers?</h3>
       <div className="flex-row">
         <Button active onClick={() => onConfirm()}>

1de89df70460d4e6aaa9437a38c516761a337829
Isuu1 <jakub.babelek@gmail.com>
2025-03-04 18:49:48 +0000

    Fixed intercepted page exit animation issue

--- app/@modal/(.)login/page.tsx
+++ app/@modal/(.)login/page.tsx
@@ -1,7 +1,29 @@
 "use client";
 
 import Modal from "@/components/Modal/Modal";
+import Button from "@/components/UI/Button";
+import { AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function InterceptedLogin() {
-  return <Modal>Test</Modal>;
+  const [showModal, setShowModal] = useState<boolean>(true);
+
+  const router = useRouter();
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  return (
+    <div>
+      <AnimatePresence mode="wait" onExitComplete={() => router.back()}>
+        {showModal && (
+          <Modal key="modal">
+            <Button onClick={() => closeModal()}>X</Button>
+          </Modal>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 }

a224cc46b99940f80dd0e3e767cd64bbd25c10da
Isuu1 <jakub.babelek@gmail.com>
2025-03-05 12:08:51 +0000

    Added dynamic input component

---
+++ components/UI/Input.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+//Styles
+import styles from "./Input.module.css";
+
+interface IProps {
+  id: string;
+  label: string;
+  type: string;
+  labelHidden: boolean;
+  required?: boolean;
+  background: "light" | "dark";
+}
+
+const Input: React.FC<IProps> = ({
+  label,
+  labelHidden,
+  id,
+  type,
+  background,
+  required,
+}) => {
+  return (
+    <div>
+      <label htmlFor={label}>{!labelHidden && label}</label>
+      <input
+        className={`${styles.input} ${
+          background === "light" ? styles.light : styles.dark
+        }`}
+        required={required}
+        id={id}
+        name={id}
+        type={type}
+      />
+    </div>
+  );
+};
+
+export default Input;

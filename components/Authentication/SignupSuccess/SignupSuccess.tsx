fbbf687d25c9ccad38e62606493773f7fe7463a2
Isuu1 <jakub.babelek@gmail.com>
2025-03-06 16:47:02 +0000

    Fix to signup form succes email link

--- components/Authentication/SignupSuccess/SignupSuccess.tsx
+++ components/Authentication/SignupSuccess/SignupSuccess.tsx
@@ -17,15 +17,15 @@ const SignupSuccess: React.FC<IProps> = ({ email, successMessage }) => {
     const domain = email.split("@")[1];
     switch (domain) {
       case "gmail.com":
-        return `"https://mail.google.com/"`;
+        return "https://mail.google.com/";
       case "yahoo.com":
-        return `"https://mail.yahoo.com/"`;
+        return "https://mail.yahoo.com/";
       case "outlook.com":
       case "hotmail.com":
       case "live.com":
-        return `"https://outlook.live.com/"`;
+        return "https://outlook.live.com/";
       default:
-        return `mailto:${email}`; //If there is no match, return dafault provider
+        return "mailto:${email}"; //If there is no match, return dafault provider
     }
   };
 

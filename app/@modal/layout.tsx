20bf23ba7b773b080d60c53eb0533c35bc4b4d00
Isuu1 <jakub.babelek@gmail.com>
2025-03-09 13:06:58 +0000

    Added main layout for intercepted modal routes

---
+++ app/@modal/layout.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+//Components
+import Modal from "@/components/Modal/Modal";
+import Button from "@/components/UI/Button";
+
+//Animations
+import { AnimatePresence } from "framer-motion";
+
+export default function ModalLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [showModal, setShowModal] = useState<boolean>(true);
+
+  const router = useRouter();
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      {/* Using onExitComplete here is necessary to keep modal exit animation working */}
+      <AnimatePresence mode="wait" onExitComplete={() => router.back()}>
+        {showModal && (
+          <Modal key="modal" theme="light">
+            <div className="margin-left-auto">
+              <Button onClick={() => closeModal()}>X</Button>
+            </div>
+            {children}
+          </Modal>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
--- components/Authentication/LoginForm/LoginForm.tsx
+++ components/Authentication/LoginForm/LoginForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useActionState, useEffect, useState } from "react";
 
 //Components

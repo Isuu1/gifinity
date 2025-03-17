72de9a468bb207b6e9f9dc361f1a343798c115ba
Isuu1 <jakub.babelek@gmail.com>
2025-03-17 11:55:18 +0000

    Added animation to media type menu

--- components/MediaTypeMenu/MediaTypeMenu.tsx
+++ components/MediaTypeMenu/MediaTypeMenu.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Button from "../UI/Button";
 //Styles
 import styles from "./MediaTypeMenu.module.scss";
+//Animations
+import { motion } from "framer-motion";
 
 interface IProps {
   activeButton: string;
@@ -12,18 +14,27 @@ interface IProps {
 const MediaTypeMenu: React.FC<IProps> = ({ activeButton, setActiveButton }) => {
   return (
     <div className={styles.mediaTypeMenu}>
-      <Button
-        active={activeButton === "gifs" && true}
-        onClick={() => setActiveButton("gifs")}
-      >
-        Gifs
-      </Button>
-      <Button
-        active={activeButton === "stickers" && true}
-        onClick={() => setActiveButton("stickers")}
-      >
-        Stickers
-      </Button>
+      <div className={styles.buttonsContainer}>
+        <motion.div
+          className={styles.indicator}
+          animate={{ x: activeButton === "gifs" ? 0 : "100%" }}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+        ></motion.div>
+        <Button
+          className={styles.button}
+          // active={activeButton === "gifs" && true}
+          onClick={() => setActiveButton("gifs")}
+        >
+          Gifs
+        </Button>
+        <Button
+          className={styles.button}
+          // active={activeButton === "stickers" && true}
+          onClick={() => setActiveButton("stickers")}
+        >
+          Stickers
+        </Button>
+      </div>
     </div>
   );
 };

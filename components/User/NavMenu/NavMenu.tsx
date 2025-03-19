f5811793e782e82777d46374b602e2d907e8f24f
Isuu1 <jakub.babelek@gmail.com>
2025-03-19 13:02:07 +0000

    Added active element to nav menu

--- components/User/NavMenu/NavMenu.tsx
+++ components/User/NavMenu/NavMenu.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 //Styles
 import styles from "./NavMenu.module.scss";
@@ -8,9 +12,10 @@ import { FaUserAlt } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
 import { FaSignOutAlt } from "react-icons/fa";
-import Image from "next/image";
 
 const NavMenu: React.FC = () => {
+  const pathname = usePathname();
+
   return (
     <nav>
       <ul className={styles.navMenu}>
@@ -26,15 +31,27 @@ const NavMenu: React.FC = () => {
           <h3>@Username</h3>
         </div>
 
-        <li className={styles.item}>
+        <li
+          className={`${styles.item} ${
+            pathname === "/user/profile" && styles.active
+          }`}
+        >
           <FaUserAlt />
           Profile
         </li>
-        <li className={styles.item}>
+        <li
+          className={`${styles.item} ${
+            pathname === "/user/favourites" && styles.active
+          }`}
+        >
           <FaHeart />
           Favourites
         </li>
-        <li className={styles.item}>
+        <li
+          className={`${styles.item} ${
+            pathname === "/user/details" && styles.active
+          }`}
+        >
           <IoMdSettings />
           Account details
         </li>
--- components/User/Profile.tsx
+++ components/User/Profile.tsx
@@ -4,16 +4,7 @@ import React from "react";
 const Profile: React.FC = () => {
   return (
     <div>
-      <h2>Profile</h2>
-      {/* <div>
-        <Image
-          src="/images/avatar.svg"
-          width={100}
-          height={100}
-          alt="avatar"
-          priority
-        />
-      </div> */}
+      <h1>Profile</h1>
       <h3>My work</h3>
       <p>You did not uploaded any gifs or stickers yet</p>
     </div>

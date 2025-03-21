a5a0f1800b872ba59e39c8390a2138fd01effc86
Isuu1 <jakub.babelek@gmail.com>
2025-03-21 11:01:46 +0000

    Added function to add stickers to user favourites in db

--- utils/user/saveFavouriteMediaToDb.ts
+++ utils/user/saveFavouriteMediaToDb.ts
@@ -25,6 +25,14 @@ export async function saveFavouriteMediaToDb(media: Gif | Sticker) {
       .eq("id", userId)
       .single();
 
+    //Select the user's profile favourite stickers table
+    const { data: userFavouriteStickers } = await supabase
+      .from("profiles")
+      .select("favourite_stickers")
+      .eq("id", userId)
+      .single();
+
+    //Add Gif to favourites
     if (media.type === "gif") {
       // Check if the gif is already in the user's favourite gifs
       const isGifInFavourites = userFavouriteGifs?.favourite_gifs.data.some(
@@ -63,7 +71,50 @@ export async function saveFavouriteMediaToDb(media: Gif | Sticker) {
         };
       }
 
-      return { success: "Favourite gif added", status: 200 };
+      return { success: "Gif added to wishlist", status: 200 };
+    }
+
+    //Add Sticker to favourites
+    if (media.type === "sticker") {
+      // Check if the gif is already in the user's favourite gifs
+      const isStickerInFavourites =
+        userFavouriteStickers?.favourite_stickers.data.some(
+          (sticker: Sticker) => sticker.id === media.id
+        );
+
+      //Create a new variable to hold the updated favourite gifs
+      //Has to be declared here to be accessible outside the if statement
+      let updatedFavouriteStickers;
+
+      if (isStickerInFavourites) {
+        // Remove the gif from the user's favourite gifs
+        updatedFavouriteStickers = {
+          data: userFavouriteStickers?.favourite_stickers.data.filter(
+            (sticker: Sticker) => sticker.id !== media.id
+          ),
+        };
+      } else {
+        // Update the favouriteStickers array
+        updatedFavouriteStickers = {
+          data: [...userFavouriteStickers?.favourite_stickers.data, media],
+        };
+      }
+
+      //Update the user's metadata with the new favouriteGifs array
+      const { error: updateError } = await supabase
+        .from("profiles")
+        .update({ favourite_stickers: updatedFavouriteStickers })
+        .eq("id", userId)
+        .single();
+
+      if (updateError) {
+        return {
+          error: `Error updating favourites, ${updateError}`,
+          status: 500,
+        };
+      }
+
+      return { success: "Favourite sticker added", status: 200 };
     }
   } catch (error) {
     return { error: `Error updating favourites, ${error}`, status: 500 };

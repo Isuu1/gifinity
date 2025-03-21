191f2c9b8d2738434742fcb09171cb0e16c8f0b4
Isuu1 <jakub.babelek@gmail.com>
2025-03-21 11:12:32 +0000

    Added notification when user add/remove media from favourites

--- components/MediaOverlay/FavouriteButtonLoggedIn/FavouriteButtonLoggedIn.tsx
+++ components/MediaOverlay/FavouriteButtonLoggedIn/FavouriteButtonLoggedIn.tsx
@@ -20,6 +20,8 @@ import { useAuth } from "@/context/AuthContext";
 
 //Styles
 import styles from "./FavouriteButtonLoggedIn.module.scss";
+import toast from "react-hot-toast";
+import { toastStyle } from "@/styles/toast";
 
 interface IProps {
   media: Gif | Sticker;
@@ -28,18 +30,42 @@ interface IProps {
 const FavouriteButtonLoggedIn: React.FC<IProps> = ({ media }) => {
   const { fetchUser, favouriteGifs, favouriteStickers } = useAuth();
 
-  const handleAddToDb = async () => {
-    const result = await saveFavouriteMediaToDb(media);
-    //Refetch the user data to update the favourite gifs and stickers on client
-    if (result?.success) fetchUser();
-  };
-
   const isGifOnWishlist = favouriteGifs.data.some(
     (gif: Gif) => gif.id === media.id
   );
   const isStickerOnWishlist = favouriteStickers.data.some(
     (sticker: Sticker) => sticker.id === media.id
   );
+
+  const handleAddToDb = async () => {
+    const result = await saveFavouriteMediaToDb(media);
+    //Refetch the user data to update the favourite gifs and stickers on client
+    if (result?.success) fetchUser();
+    // if (result?.success && !isGifOnWishlist && media.type === "gif") {
+    //   toast.success("Gif added to wishlist", toastStyle);
+    // } else if (result?.success && isGifOnWishlist) {
+    //   toast.success("Gif removed from wishlist", toastStyle);
+    // }
+    // if (result?.success && !isStickerOnWishlist && media.type === "sticker") {
+    //   toast.success("Sticker added to wishlist", toastStyle);
+    // } else if (result?.success && isStickerOnWishlist) {
+    //   toast.success("Sticker removed from wishlist", toastStyle);
+    // }
+    if (result?.success && media.type === "gif") {
+      toast.success(
+        isGifOnWishlist ? "Gif removed from wishlist" : "Gif added to wishlist",
+        toastStyle
+      );
+    }
+    if (result?.success && media.type === "sticker") {
+      toast.success(
+        isStickerOnWishlist
+          ? "Sticker removed from wishlist"
+          : "Sticker added to wishlist",
+        toastStyle
+      );
+    }
+  };
 
   return (
     <motion.div

70f7de71025061a7a4c714c34a7bc36053a16c4c
Isuu1 <jakub.babelek@gmail.com>
2025-03-28 15:59:33 +0000

    Fixed issues with module new paths

--- features/collections/components/AddToCollectionForm.tsx
+++ features/collections/components/AddToCollectionForm.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const Form = () => {
   return (
-    <div className={styles.newCollectionContainer}>
-      <Form action={formAction}>
-        <Input id="name" type="text" label="name" variant="light" />
-        <Button variant="light" type="submit">
-          Create
-        </Button>
-      </Form>
-    </div>
+    // <div className={styles.newCollectionContainer}>
+    //   <Form action={formAction}>
+    //     <Input id="name" type="text" label="name" variant="light" />
+    //     <Button variant="light" type="submit">
+    //       Create
+    //     </Button>
+    //   </Form>
+    // </div>
+    <div>test</div>
   );
 };
 

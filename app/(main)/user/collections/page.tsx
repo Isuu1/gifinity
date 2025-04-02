6800c7e4c2f0dcf99f477967f280d00e863ba9ea
Isuu1 <jakub.babelek@gmail.com>
2025-04-02 20:21:33 +0100

    Added page headline to collections page

--- app/(main)/user/collections/page.tsx
+++ app/(main)/user/collections/page.tsx
@@ -1,9 +1,10 @@
+import PageHeadline from "@/components/PageHeadline/PageHeadline";
 import CollectionsGrid from "@/features/collections/components/CollectionsGrid";
 
 export default function Page() {
   return (
     <div>
-      <h1>Collections</h1>
+      <PageHeadline title="Collections" imageUrl="/images/collection.svg" />
       <CollectionsGrid />
     </div>
   );

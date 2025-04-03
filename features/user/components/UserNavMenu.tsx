f2a00b71f3e01e2f9d686633310b6bd2f6de40d0
Isuu1 <jakub.babelek@gmail.com>
2025-04-03 10:01:14 +0100

    Added signout functionality to user nav menu

--- features/user/components/UserNavMenu.tsx
+++ features/user/components/UserNavMenu.tsx
@@ -3,24 +3,38 @@
 import React from "react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
+import toast from "react-hot-toast";
 
 //Styles
 import styles from "./UserNavMenu.module.scss";
-
+import { toastStyle } from "@/styles/toast";
 //Icons
 import { FaUserAlt } from "react-icons/fa";
 import { IoBookmarks } from "react-icons/io5";
-
 import { IoSettings } from "react-icons/io5";
-
 import { FaSignOutAlt } from "react-icons/fa";
-import Link from "next/link";
+//Providers
 import { useAuth } from "@/context/AuthContext";
+//Actions
+import { signout } from "@/features/auth/lib/actions/signout";
 
 const NavMenu: React.FC = () => {
   const pathname = usePathname();
 
   const { avatar, username } = useAuth();
+
+  const handleSignOut = async () => {
+    const result = await signout();
+    if (result?.error) {
+      toast.error(result.error);
+    } else {
+      toast.success("You've safely signed out.", toastStyle);
+      setTimeout(() => {
+        window.location.pathname = "/";
+      }, 1000);
+    }
+  };
 
   return (
     <nav>
@@ -66,7 +80,7 @@ const NavMenu: React.FC = () => {
             Account details
           </li>
         </Link>
-        <li className={styles.item}>
+        <li className={styles.item} onClick={handleSignOut}>
           <FaSignOutAlt />
           Sign out
         </li>

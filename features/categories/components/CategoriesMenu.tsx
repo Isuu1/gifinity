a3d2f8a2745cb2b69d34d776f1768573fd8eef36
Isuu1 <jakub.babelek@gmail.com>
2025-04-03 17:39:25 +0100

    Error and loading states in categories menu

--- features/categories/components/CategoriesMenu.tsx
+++ features/categories/components/CategoriesMenu.tsx
@@ -5,21 +5,37 @@ import { useRouter } from "next/navigation";
 
 //Styles
 import styles from "./CategoriesMenu.module.scss";
-
 //Components
-
-//Interfaces
+import Error from "@/components/Error/Error";
+//Types
 import { Categories } from "@/interfaces/categories";
 
 const CategoriesMenu: React.FC = () => {
   const [categories, setCategories] = useState<Categories>({ data: [] });
 
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const [error, setError] = useState<string | null>(null);
+
+  console.log(categories);
+
   useEffect(() => {
     async function fetchData() {
-      const data = await fetch("api/categories");
-      const response = await data.json();
+      setIsLoading(true);
+      try {
+        const data = await fetch("api/categories");
+        if (!data.ok) {
+          setError("There was an error fetching the categories.");
+          return;
+        }
+        const response = await data.json();
 
-      setCategories(response);
+        setCategories(response);
+      } catch (error) {
+        setError(error as string);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchData();
   }, []);
@@ -31,6 +47,26 @@ const CategoriesMenu: React.FC = () => {
     const encodedCategoryName = encodeURIComponent(categoryName);
     router.push(`/categories?q=${encodedCategoryName}`);
   };
+
+  if (error)
+    return (
+      <div className={styles.categoriesContainer}>
+        <Error errorMessage={error} />
+      </div>
+    );
+
+  if (isLoading)
+    return (
+      <div className={styles.categoriesContainer}>
+        <ul className={styles.categoriesWrapper}>
+          {Array.from({ length: 28 }).map((category, index: number) => (
+            <li key={index} className={styles.navItem}>
+              <span>Loading</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
 
   return (
     <div className={styles.categoriesContainer}>

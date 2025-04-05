7e6f43d62404685571575869b8b6633319fd82f6
Isuu1 <jakub.babelek@gmail.com>
2025-04-05 20:49:22 +0100

    Added UploadPageLayout component

--- app/(upload)/upload/page.tsx
+++ app/(upload)/upload/page.tsx
@@ -1,3 +1,9 @@
+import UploadPageLayout from "@/layouts/UploadPageLayout";
+
 export default function Page() {
-  return <div>upload</div>;
+  return (
+    <UploadPageLayout>
+      <h1>Upload media</h1>
+    </UploadPageLayout>
+  );
 }
---
+++ layouts/UploadPageLayout.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+// @ts-expect-error
+import WAVES from "vanta/dist/vanta.waves.min";
+// @ts-expect-error
+import * as THREE from "three";
+//Styles
+import styles from "./UploadPageLayout.module.scss";
+
+export default function UploadPageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const uploadBackgroundRef = useRef(null);
+  useEffect(() => {
+    if (typeof window !== "undefined" && !vantaEffect) {
+      setVantaEffect(
+        WAVES({
+          THREE: THREE,
+          el: uploadBackgroundRef.current,
+          color: 0x870922,
+        })
+      );
+    }
+    return () => {
+      // @ts-expect-error
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+  return (
+    <div className={styles.uploadPageLayout} ref={uploadBackgroundRef}>
+      <div className={styles.container}>
+        <h1>Upload media</h1>
+        {children}
+      </div>
+    </div>
+  );
+}

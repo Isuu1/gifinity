98cb91133cb026d5e5eff3df45efeb05464b5809
Isuu1 <jakub.babelek@gmail.com>
2025-04-06 15:22:24 +0100

    Removed old code from upload page

--- app/(upload)/upload/page.tsx
+++ app/(upload)/upload/page.tsx
@@ -1,13 +1,6 @@
 import Upload from "@/features/upload/components/Upload";
 
 export default async function Page() {
-  // const trendingGifsResponse = await fetch(
-  //   `${process.env.NEXT_PUBLIC_SITE_URL}/api/trending/gifs`
-  // );
-
-  // const trendingGifs = await trendingGifsResponse.json();
-  // console.log("trendingGifs", trendingGifs);
-
   return (
     <div className="page">
       <Upload />

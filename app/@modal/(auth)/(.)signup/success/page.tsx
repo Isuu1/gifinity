d688395a8950a1b7f8d00d94ef96a40be6b2525a
Isuu1 <jakub.babelek@gmail.com>
2025-04-10 18:19:48 +0100

    Added separate page /signup/success

---
+++ app/@modal/(auth)/(.)signup/success/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import SignupSuccess from "@/features/auth/components/SignupSuccess";
+import { useSearchParams } from "next/navigation";
+
+export default function InterceptedSignupSuccess() {
+  const params = useSearchParams();
+  const email = params.get("email");
+
+  if (!email) {
+    // Redirect to home if email is not present in the URL
+    window.location.href = "/";
+    return null;
+  }
+
+  return <SignupSuccess email={email} />;
+}
--- features/auth/components/SignupForm.tsx
+++ features/auth/components/SignupForm.tsx
@@ -2,15 +2,14 @@
 
 import React, { useEffect, useState } from "react";
 import { useActionState } from "react";
+import { useRouter } from "next/navigation";
 
 //Components
 import Button from "@/components/UI/Button";
 import Form from "@/components/UI/Form";
 import Input from "@/components/UI/Input";
-import SignupSuccess from "./SignupSuccess";
 import AuthError from "./AuthError";
 import AuthProviders from "./AuthProviders";
-
 //Icons
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
@@ -18,14 +17,11 @@ import { IoMdEye } from "react-icons/io";
 import { IoMdEyeOff } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
-
 //Styles
 import styles from "./SignupForm.module.scss";
-
 //Utils
 import { signup } from "@/features/auth/actions/auth";
 import { normalizeErrors } from "@/features/auth/utils/authHelpers";
-
 //Types
 import { SignupFormState } from "../types/forms";
 
@@ -44,6 +40,8 @@ const SignupForm: React.FC = () => {
 
   const [state, formAction, isPending] = useActionState(signup, initialState);
 
+  const router = useRouter();
+
   //Set error message whenever form state returns one
   useEffect(() => {
     if (state.error) {
@@ -52,6 +50,13 @@ const SignupForm: React.FC = () => {
     }
   }, [state.resetKey, state.error]);
 
+  useEffect(() => {
+    //If signup is successful, redirect to success page with email as query param
+    if (state.success) {
+      router.push(`/signup/success?email=${state.data.email}`);
+    }
+  }, [state.success, router, state.data.email]);
+
   // Function to clear the error when user focuses on an input
   const handleFocus = () => {
     setError([]);
@@ -59,100 +64,89 @@ const SignupForm: React.FC = () => {
 
   return (
     <div className={styles.signupFormContainer}>
-      {state.success && (
-        <SignupSuccess
-          email={state.data.email}
-          successMessage="Your account has been created!"
+      <h3 className={styles.signupFormDescription}>
+        Create account to access your collections, sync across devices, and
+        more!
+      </h3>
+
+      <Form action={formAction}>
+        <Input
+          type="email"
+          id="email"
+          label="Email"
+          required
+          disabled={isPending}
+          labelHidden
+          placeholder="Email"
+          icon={<MdEmail />}
+          defaultValue={state.data?.email}
+          onFocus={handleFocus}
         />
-      )}
+        <Input
+          type="username"
+          id="username"
+          label="Username"
+          required
+          disabled={isPending}
+          labelHidden
+          placeholder="Username"
+          icon={<FaUser />}
+          onFocus={handleFocus}
+        />
+        <Input
+          type={showPassword ? "text" : "password"}
+          id="password"
+          label="Password"
+          required
+          disabled={isPending}
+          labelHidden
+          placeholder="Password"
+          icon={<RiLockPasswordFill />}
+          showPasswordIcon={
+            showPassword ? (
+              <IoMdEye onClick={() => setShowPassword(false)} />
+            ) : (
+              <IoMdEyeOff onClick={() => setShowPassword(true)} />
+            )
+          }
+          onFocus={handleFocus}
+        />
+        <Input
+          type={showPassword ? "text" : "password"}
+          id="confirmPassword"
+          label="confirmPassword"
+          required
+          disabled={isPending}
+          labelHidden
+          placeholder="Confirm password"
+          icon={<RiLockPasswordFill />}
+          showPasswordIcon={
+            showPassword ? (
+              <IoMdEye onClick={() => setShowPassword(false)} />
+            ) : (
+              <IoMdEyeOff onClick={() => setShowPassword(true)} />
+            )
+          }
+          onFocus={handleFocus}
+        />
 
-      {!state.success && (
-        <>
-          <h3 className={styles.signupFormDescription}>
-            Create account to access your collections, sync across devices, and
-            more!
-          </h3>
+        {error.length > 0 && <AuthError key="error" error={error} />}
 
-          <Form action={formAction}>
-            <Input
-              type="email"
-              id="email"
-              label="Email"
-              required
-              disabled={isPending}
-              labelHidden
-              placeholder="Email"
-              icon={<MdEmail />}
-              defaultValue={state.data?.email}
-              onFocus={handleFocus}
-            />
-            <Input
-              type="username"
-              id="username"
-              label="Username"
-              required
-              disabled={isPending}
-              labelHidden
-              placeholder="Username"
-              icon={<FaUser />}
-              onFocus={handleFocus}
-            />
-            <Input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              label="Password"
-              required
-              disabled={isPending}
-              labelHidden
-              placeholder="Password"
-              icon={<RiLockPasswordFill />}
-              showPasswordIcon={
-                showPassword ? (
-                  <IoMdEye onClick={() => setShowPassword(false)} />
-                ) : (
-                  <IoMdEyeOff onClick={() => setShowPassword(true)} />
-                )
-              }
-              onFocus={handleFocus}
-            />
-            <Input
-              type={showPassword ? "text" : "password"}
-              id="confirmPassword"
-              label="confirmPassword"
-              required
-              disabled={isPending}
-              labelHidden
-              placeholder="Confirm password"
-              icon={<RiLockPasswordFill />}
-              showPasswordIcon={
-                showPassword ? (
-                  <IoMdEye onClick={() => setShowPassword(false)} />
-                ) : (
-                  <IoMdEyeOff onClick={() => setShowPassword(true)} />
-                )
-              }
-              onFocus={handleFocus}
-            />
+        <Button
+          variant="light"
+          type="submit"
+          disabled={isPending}
+          className={styles.signupButton}
+          icon={<IoSend />}
+          iconPosition="right"
+        >
+          {isPending ? "Creating account..." : "Create account"}
+        </Button>
+      </Form>
 
-            {error.length > 0 && <AuthError key="error" error={error} />}
+      <h4>——— or ———</h4>
 
-            <Button
-              variant="light"
-              type="submit"
-              disabled={isPending}
-              className={styles.signupButton}
-              icon={<IoSend />}
-              iconPosition="right"
-            >
-              {isPending ? "Creating account..." : "Create account"}
-            </Button>
-          </Form>
-
-          <h4>——— or ———</h4>
-
-          <AuthProviders />
-        </>
-      )}
+      <AuthProviders />
     </div>
   );
 };
